7ebd30f5818867a2f120d302f36a2f8e0c0e06ff
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2021-03-24 18:24:30 +0200

    remove wait regen

--- GameControl/src/ru/boomearo/gamecontrol/runnable/AbstractPool.java
+++ GameControl/src/ru/boomearo/gamecontrol/runnable/AbstractPool.java
@@ -3,9 +3,6 @@ package ru.boomearo.gamecontrol.runnable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-import ru.boomearo.gamecontrol.GameControl;
 
 public abstract class AbstractPool {
 
@@ -19,13 +16,14 @@ public abstract class AbstractPool {
         this.pool.execute(task);
     }
     
+    //TODO когда пофиксят FAWE тогда будем ожидать в основном потоке
     public void stop() throws InterruptedException {
-        long start = System.currentTimeMillis();
-        GameControl.getInstance().getLogger().info("Ожидаем закрытия пула регенерации арен..");
-        this.pool.shutdown();
-        this.pool.awaitTermination(2, TimeUnit.MINUTES);
-        long end = System.currentTimeMillis();
-        GameControl.getInstance().getLogger().info("Пул регенераций закрыт за " + (end - start) + "мс");
+        //long start = System.currentTimeMillis();
+        //GameControl.getInstance().getLogger().info("Ожидаем закрытия пула регенерации арен..");
+        //this.pool.shutdown();
+        //this.pool.awaitTermination(2, TimeUnit.MINUTES);
+        //long end = System.currentTimeMillis();
+        //GameControl.getInstance().getLogger().info("Пул регенераций закрыт за " + (end - start) + "мс");
     }
     
 }
