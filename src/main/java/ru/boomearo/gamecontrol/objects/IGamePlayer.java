bf450eb0b387f9328c02f441601146e6a1193e70
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-02-04 18:27:40 +0200

    Provide factory in sendBoard

--- src/main/java/ru/boomearo/gamecontrol/objects/IGamePlayer.java
+++ src/main/java/ru/boomearo/gamecontrol/objects/IGamePlayer.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 
 import ru.boomearo.board.Board;
 import ru.boomearo.board.exceptions.BoardException;
+import ru.boomearo.board.objects.IPageListFactory;
 import ru.boomearo.board.objects.PlayerBoard;
 import ru.boomearo.board.objects.boards.AbstractPageList;
 import ru.boomearo.gamecontrol.objects.arena.AbstractGameArena;
@@ -32,22 +33,24 @@ public interface IGamePlayer {
 
     /**
      * Устанавливает правую панель статистики этому игроку.
-     * @param pageList Панель статистики. Если указан null, панель будет сброшена.
+     * @param factory Завод по созданию панели статистики. Если указан null, панель будет сброшена.
      */
-    public default void sendBoard(AbstractPageList pageList) {
+    public default void sendBoard(IPageListFactory factory) {
         PlayerBoard pb = Board.getInstance().getBoardManager().getPlayerBoard(getName());
         if (pb != null) {
             try {
-                if (pageList == null) {
+                if (factory == null) {
                     pb.setNewPageList(Board.getInstance().getBoardManager().getPageListFactory().createPageList(pb));
                     return;
                 }
 
-                pb.setNewPageList(pageList);
+                pb.setNewPageList(factory.createPageList(pb));
             }
             catch (BoardException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
 }
